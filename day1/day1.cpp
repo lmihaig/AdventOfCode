936567dd7346f03213f9a8dc7752f5ffaa513f7c
lmihaig <licu.mihai21@gmail.com>
2020-12-04 15:21:21 +0200

    init

---
+++ day1/day1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+ifstream input("day1.in");
+
+int main()
+{
+    unsigned int x, i, j, m, size;
+    vector<unsigned int> expense;
+    while(input>>x)
+    {
+        expense.push_back(x);
+    }
+
+size = expense.size();
+
+//two entries
+    for(i = 0; i < size; i++)
+        for(j = i; j < size; j++)
+            {
+                if(expense[i]+expense[j] == 2020) 
+                {
+                    cout<<expense[i]*expense[j]<<'\n';
+                }
+            }
+
+//three entries
+    for(i = 0; i < size; i++)
+        for(j = i; j < size; j++)
+            for(m = j; m < size; m++)
+            {
+                if(expense[i]+expense[j]+expense[m] == 2020) 
+                {
+                    cout<<expense[i]*expense[j]*expense[m]<<'\n';
+                }
+            }      
+}
