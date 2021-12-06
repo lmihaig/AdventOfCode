832ecf5b6fed8ac7feeeecddd6384ec1dd03531a
lmihaig <licu.mihai21@gmail.com>
2021-12-06 20:20:35 +0200

    ready for day7

---
+++ AoC2015/day1/target/rls/debug/deps/day1-27a461679cfd396d.d
@@ -0,0 +1,5 @@
+d:\Code\AdventOfCode\AoC2015\day1\target\rls\debug\deps\day1-27a461679cfd396d.rmeta: src\main.rs
+
+d:\Code\AdventOfCode\AoC2015\day1\target\rls\debug\deps\day1-27a461679cfd396d.d: src\main.rs
+
+src\main.rs:
---
+++ AoC2021/day7/day7.py
@@ -0,0 +1,2 @@
+with open("day7.txt", 'r') as file:
+    lines = file.read().strip().split("\n")
